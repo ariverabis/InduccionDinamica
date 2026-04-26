c8a6950667c0a8fbce3438aff36bb78f83e80608
ariverabis <arivera@example.com>
2026-04-26 18:23:05 -0400

    cambios api

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://rreqcrmdyrgevdugzurx.supabase.co'
-const supabaseAnonKey = 'sb_publishable_xJidLXDtiL2N9eUniwq5wg_ewAAZUPD'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyZXFjcm1keXJnZXZkdWd6dXJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNDIxNTcsImV4cCI6MjA4ODYxODE1N30.4OIG_NMwzCmkhgFJlf69dMP7S276wa7wezzEg4gPWOY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
